09e3a981e761536276d105f341412c4c486482fa
skillzTheProgrammer <47523568+skillzTheProgrammer@users.noreply.github.com>
2024-07-07 12:59:06 +0100

    added sessions table

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
      return response()-> json([
+            "message"=>"Welcome to Buckhill API Test",
             "success" => true,
             "statusCode" => 200
     ]);
